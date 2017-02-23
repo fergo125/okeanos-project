561249116b036f43b853270c8b2be6aebe158022
Fernando Mata <ffmm.14@gmail.com>
2017-02-23 15:57:49 -0600

    Started coding the main script

--- okeanos.py
+++ okeanos.py
@@ -5,11 +5,25 @@ import time
 import datetime
 from mpl_toolkits.basemap import Basemap, shiftgrid
 import numpy as np
-import matplotlib.pyplot as plt
 import os
 import sys
+import map_creator
+
+params = None
+dataset = None
 
 def main():
-    
+    xml= open(sys.argv[1],"r").read()
+    params = untangle.parse(xml)
+    dataset = netCDF4.dataset(params.template.file,"r")
+
+    if params['type'] == "gif":
+        create_gif()
+
 if __name__=="main":
     main()
+
+def create_gif():
+    dataset_vars = list()
+    for var in params.template.variables:
+        dataset_vars.append(dataset[])
