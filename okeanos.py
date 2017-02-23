611bb98b024ba0e9c8d8f683b82f9675fd0a700a
Fernando Mata <ffmm.14@gmail.com>
2017-02-23 14:27:27 -0600

    Added xml configuration example

---
+++ okeanos.py
@@ -0,0 +1,15 @@
+import untangle
+import netCDF4
+from netCDF4 import num2date, date2num
+import time
+import datetime
+from mpl_toolkits.basemap import Basemap, shiftgrid
+import numpy as np
+import matplotlib.pyplot as plt
+import os
+import sys
+
+def main():
+    
+if __name__=="main":
+    main()
