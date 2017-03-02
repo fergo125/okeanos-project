46f06b4f5466622a9ce88102482519fbb833af86
Fernando Mata <ffmm.14@gmail.com>
2017-03-02 11:13:38 -0600

    Added changes to script

--- okeanos.py
+++ okeanos.py
@@ -6,9 +6,11 @@ import datetime
 from mpl_toolkits.basemap import Basemap, shiftgrid
 import numpy as np
 import os
-import sys
 import map_creator
+import sys
 
+reload(sys) # just to be sure
+sys.setdefaultencoding('utf-8')
 params = None
 dataset = None
 
@@ -16,8 +18,7 @@ def main():
     xml= open(sys.argv[1],"r").read()
     params = untangle.parse(xml)
     dataset = netCDF4.dataset(params.template.file,"r")
-
-    if params['type'] == "gif":
+    if params.template['type'] == "gif":
         create_gif()
 
 if __name__=="main":
@@ -25,5 +26,24 @@ if __name__=="main":
 
 def create_gif():
     dataset_vars = list()
-    for var in params.template.variables:
-        dataset_vars.append(dataset[])
+    dataset_layers = list()
+
+    latitude_array = dataset[params.template.variables.lat["var_name"]]
+    longitude_array = dataset[params.template.variables.lon["var_name"]]
+
+    template_max_longitude = params.template.layers["max_lon"]
+    template_max_latitude = params.template.layers["max_lat"]
+    template_min_longitude = params.template.layers["min_lon"]
+    template_min_latitude = params.template.layers["min_lat"]
+
+    if template_min_latitude >= latitude_array[0] and \
+        template_min_longitude >= longitude_array[0] and \
+        template_max_latitude <= latitude_array[len(latitude_array)-1] and \
+        template_max_latitude <= latitude_array[len(longitude_array)-1]:
+        for var in params.template.variables.var:
+            dataset_vars.append(dataset[var['var_name']])
+        for layer in params.template.layers.layer:
+            
+    else:
+        print('Bad template boundaries')
+        return 0
