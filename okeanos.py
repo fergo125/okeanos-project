e59cdc47a2276b70ad959c933f765d028b1c31e7
miocimar <miocimar@miocimarCaspio.local>
2017-03-02 15:27:39 -0600

    Refractored code in order to use classes.

--- okeanos.py
+++ okeanos.py
@@ -11,39 +11,62 @@ import sys
 
 reload(sys) # just to be sure
 sys.setdefaultencoding('utf-8')
-params = None
-dataset = None
+
+class  Okeanos(object):
+    """docstring for  Okeanos."""
+    def __init__(self, params, dataset):
+        self.params = params
+        self.dataset = dataset
+
+        self.dataset_vars = list()
+        self.latitude_array = self.dataset[params.template.variables.lat["var_file_name"]]
+        self.longitude_array = self.dataset[params.template.variables.lon["var_file_name"]]
+
+        self.template_dimensions=list()
+
+    def launch(self):
+        if self.params.template['type'] == "gif":
+            self.create_gif()
+
+    def validate(self):
+        self.template_dimensions.append(int(self.params.template.layers["max_lat"]))
+        self.template_dimensions.append(int(self.params.template.layers["max_lon"]))
+        self.template_dimensions.append(int(self.params.template.layers["min_lat"]))
+        self.template_dimensions.append(int(self.params.template.layers["min_lon"]))
+
+        if (self.template_dimensions[0] <= self.latitude_array[len(self.latitude_array)-1] and \
+            self.template_dimensions[1] <= self.longitude_array[len(self.longitude_array)-1] and \
+            self.template_dimensions[2] >= self.latitude_array[0] and \
+            self.template_dimensions[3] >= self.longitude_array[0]) is False:
+            print("Bad template dimensions")
+            return False
+
+        for var in self.params.template.variables.var:
+            if var['var_file_name'] in self.dataset.variables:
+                self.dataset_vars.append(self.dataset[var['var_file_name']])
+            else:
+                print('Var not found in dataset: ',var['var_file_name'])
+                return False
+        return True
+
+    def create_gif(self):
+        pass
+            #for layer in params.template.layers.layer:
 
 def main():
     xml= open(sys.argv[1],"r").read()
+    print("Params readed")
     params = untangle.parse(xml)
-    dataset = netCDF4.dataset(params.template.file,"r")
-    if params.template['type'] == "gif":
-        create_gif()
+    print("Cargando Archivo:", params.template.datasource.cdata)
+    dataset = netCDF4.Dataset(params.template.datasource.cdata,"r")
+    print("dataset readed")
 
-if __name__=="main":
-    main()
+    okeanos = Okeanos(params,dataset)
 
-def create_gif():
-    dataset_vars = list()
-    dataset_layers = list()
-
-    latitude_array = dataset[params.template.variables.lat["var_name"]]
-    longitude_array = dataset[params.template.variables.lon["var_name"]]
-
-    template_max_longitude = params.template.layers["max_lon"]
-    template_max_latitude = params.template.layers["max_lat"]
-    template_min_longitude = params.template.layers["min_lon"]
-    template_min_latitude = params.template.layers["min_lat"]
-
-    if template_min_latitude >= latitude_array[0] and \
-        template_min_longitude >= longitude_array[0] and \
-        template_max_latitude <= latitude_array[len(latitude_array)-1] and \
-        template_max_latitude <= latitude_array[len(longitude_array)-1]:
-        for var in params.template.variables.var:
-            dataset_vars.append(dataset[var['var_name']])
-        for layer in params.template.layers.layer:
-            
+    if okeanos.validate():
+        okeanos.launch()
     else:
-        print('Bad template boundaries')
-        return 0
+        print("Error processing parameters")
+
+if __name__=="__main__":
+    main()
