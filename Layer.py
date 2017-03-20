1f37393b9e67343dd9e441ee897b2d35e8b00a89
miocimar <miocimar@miocimarCaspio.local>
2017-03-20 16:27:32 -0600

    Added new file that handles animation layers

---
+++ Layer.py
@@ -0,0 +1,13 @@
+class Layer(object):
+
+    def __init__(self, layer_dimensions_indexes):
+        self.type
+        self.dimensions_indexes = layer_dimensions
+        self.sub_indexes = None
+
+    def get_sub_area(self, data, precision):
+
+        max_lat_index = self.dimensions = base_lat
+        return data_sub_set[]
+
+    
